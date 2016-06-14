7f811ec8432942229150675616505474a344c051
Christine L. Feaster <feasterc@gmail.com>
2016-06-13 22:52:43 -0700

    Add pseudocode for Exercise#marklar

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,13 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    # get words in str
+    # for each word, check if greater than 4 char
+      # if word is 4 char or less, add intact word to new string
+      # if word is greater than 4 char, check if it's capitalized
+        # if word is capitalized, replace with "Marklar"
+        # if word is not capitalized, replace with "marklar"
+    # return marklar-ified string
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
